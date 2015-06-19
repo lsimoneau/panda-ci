4c38549118e72b0e8cd64a57abf1028a411bf920
Louis Simoneau <simoneau.louis@gmail.com>
2015-06-19 23:48:05 +1000

    Handle exit codes for stages
    Currently seem to be having an issue with commands that return more than
    one line of output.

--- lib/panda/stage.ex
+++ lib/panda/stage.ex
@@ -1,11 +1,36 @@
 defmodule Panda.Stage do
-  def start(parent, config) do
+  alias Porcelain.Process, as: Proc
+  alias Porcelain.Result
+
+  def execute(name, parent, config) do
+    Enum.each(config.commands, fn(command) ->
+      proc = %Proc{pid: pid} = Porcelain.spawn_shell(command, out: {:send,self()})
+      await(name, parent, pid)
+    end)
+    run(name, parent, config, 0)
   end
 
-  def run(commands) do
-    Enum.map(commands, fn(command) ->
-      %Porcelain.Result{out: out} = Porcelain.shell(command)
-      out
-    end) |> Enum.join
+  defp run(name, parent, %{ commands: [] }, status) do
+    send(parent, {name, :exit, status})
+  end
+  defp run(name, parent, config, status) when status == 0 do
+    [command|rest] = config.commands
+    proc = %Proc{pid: pid} = Porcelain.spawn_shell(command, out: {:send,self()})
+    status = await(name, parent, pid)
+    run(name, parent, %{config | commands: rest }, status)
+  end
+  defp run(name, parent, _, status) do
+    send(parent, {name, :exit, status})
+  end
+
+  defp await(name, parent, pid) do
+    receive do
+      {^pid, :data, _, data} ->
+        send(parent, {name, :output, data})
+        await(name, parent, pid)
+
+      {^pid, :result, %Result{status: status}} ->
+        status
+    end
   end
 end
--- test/panda/stage_test.exs
+++ test/panda/stage_test.exs
@@ -2,18 +2,52 @@ defmodule Panda.StageTest do
   use ExUnit.Case
 
   test "it executes a list of bash commands and sends the output back to the parent" do
-    Panda.Stage.start(self, %{ commands: [
+    Panda.Stage.execute(:test_1, self, %{ commands: [
       "echo 'hello, stage'"
     ] })
 
-    assert_receive({:ouptut, "hello, stage\n"})
-    assert_receive({:exit, 0})
+    assert_received({:test_1, :output, "hello, stage\n"})
+    assert_received({:test_1, :exit, 0})
   end
 
   test "it runs multiple commands" do
-    assert "hello, stage\nhow are you?\n" == Panda.Stage.run([
+    Panda.Stage.execute(:test_1, self, %{ commands: [
       "echo 'hello, stage'",
       "echo 'how are you?'"
-    ])
+    ] })
+
+    assert_received({:test_1, :output, "hello, stage\n"})
+    assert_received({:test_1, :output, "how are you?\n"})
+    assert_received({:test_1, :exit, 0})
+  end
+
+  test "it receives non-zero exit codes" do
+    Panda.Stage.execute(:test_1, self, %{ commands: [
+      "echo 'hello, stage'",
+      "false"
+    ] })
+
+    assert_receive({:test_1, :output, "hello, stage\n"})
+    assert_receive({:test_1, :exit, 1})
+  end
+
+  test "it stops when receiving an exit code" do
+    Panda.Stage.execute(:test_1, self, %{ commands: [
+      "echo 'hello, stage'",
+      "false",
+      "echo 'how are you?'"
+    ] })
+
+    assert_receive({:test_1, :output, "hello, stage\n"})
+    assert_receive({:test_1, :exit, 1})
+  end
+
+  test "it handles commands that return multiple lines of output" do
+    Panda.Stage.execute(:test_1, self, %{ commands: [
+      "echo 'hello, stage\nhow are you?'"
+    ] })
+
+    assert_receive({:test_1, :output, "hello, stage\n"})
+    assert_receive({:test_1, :output, "how are you?\n"})
   end
 end
